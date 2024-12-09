c37a4c91aea76f4bb8fa8b69c9e9919be9186442
NaziaImran786 <naziaimran.4df@gmail.com>
2024-12-09 14:30:05 +0500

    start

--- src/components/Leatest.tsx
+++ src/components/Leatest.tsx
@@ -1,43 +1,54 @@
-import React from 'react'
-import LeatestCard from './LeatestCard'
-import { leatestCards } from '@/constant/leatestcard'
+import React from "react";
+import LeatestCard from "./LeatestCard";
+import { leatestCards } from "@/constant/leatestcard";
 
 function LeatestProduct() {
   return (
-    < >
-    <section className='w-full 2xl:w-[1920px] mx-auto mt-[129px]  '>
-      {/* heading */}
-      <div className="container mx-auto px-4 text-center mb-[48px] ">
-        <h1 className='text-[24px] lg:text-[42px] font-bold text-[#1A0B5B] font-josefin-sans'>Leatest Products</h1>
-    </div>
-     
-     {/* links */}
-    <div className="relative hidden md:flex space-x-10 w-[527px] h-[22px] mx-auto mb-[62px]">
-      {/* New Arrival */}
-      <div className="relative">
-        <span className="font-lato font-normal text-[18px] text-[#FB2E86]">New Arrival</span>
-        <div className="absolute w-[91px] h-[1px] bg-[#FB4997] top-[24px] left-0"></div>
-      </div>
+    <>
+      <section className="w-full 2xl:w-[1920px] mx-auto mt-[129px]">
+        {/* Heading */}
+        <div className="container mx-auto px-4 text-center mb-[48px]">
+          <h1 className="text-[24px] lg:text-[42px] font-bold text-[#1A0B5B] font-josefin-sans">
+            Leatest Products
+          </h1>
+        </div>
 
-      {/* Best Seller */}
-      <span className="font-lato font-normal text-[18px] text-[#151875]">Best Seller</span>
+        {/* Links */}
+        <div className="relative hidden md:flex space-x-10 w-full justify-center mb-[62px]">
+          {/* New Arrival */}
+          <div className="relative">
+            <span className="font-lato font-normal text-[18px] text-[#FB2E86]">
+              New Arrival
+            </span>
+            <div className="absolute w-[91px] h-[1px] bg-[#FB4997] top-[24px] left-0"></div>
+          </div>
 
-      {/* Featured */}
-      <span className="font-lato font-normal text-[18px] text-[#151875]">Featured</span>
+          {/* Best Seller */}
+          <span className="font-lato font-normal text-[18px] text-[#151875]">
+            Best Seller
+          </span>
 
-      {/* Special Offer */}
-      <span className="font-lato font-normal text-[18px] text-[#151875]">Special Offer</span>
-    </div>
+          {/* Featured */}
+          <span className="font-lato font-normal text-[18px] text-[#151875]">
+            Featured
+          </span>
 
-    {/* card div */}
-    <div className="container mx-auto px-4 mb-[53px] pl-[32px]">
-    <div className='mx-auto grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 hover:scale-[1.1]'>
-    <LeatestCard loop={leatestCards}/>
-    </div>
-    </div>
-    </section>
-    </>  
-  )
+          {/* Special Offer */}
+          <span className="font-lato font-normal text-[18px] text-[#151875]">
+            Special Offer
+          </span>
+        </div>
+
+        {/* Cards */}
+        <div className="container mx-auto px-4 mb-[53px]">
+          <div className="mx-auto grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 justify-items-center">
+            <LeatestCard loop={leatestCards} />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default LeatestProduct
+export default LeatestProduct;
+
--- src/components/Trending.tsx
+++ src/components/Trending.tsx
@@ -1,49 +1,72 @@
-import React from 'react'
-import Image from 'next/image'
-import TrendingCard from './TrendingCard'
-import { TrendingCards } from '@/constant/trendingcard'
+import React from "react";
+import Image from "next/image";
+import TrendingCard from "./TrendingCard";
+import { TrendingCards } from "@/constant/trendingcard";
 
 export default function Trending() {
   return (
-    <section className='w-full 2xl:w-[1920px] mx-auto mt-[129px] mb-[130px] '>
-    {/* heading */}
-    <div className="container mx-auto px-4 text-center mb-[60px] ">
-      <h1 className='text-[24px] lg:text-[42px] font-bold text-[#1A0B5B] font-josefin-sans'>Trending Products</h1>
-    </div>
+    <section className="w-full 2xl:w-[1920px] mx-auto mt-[129px] mb-[130px]">
+      {/* Heading */}
+      <div className="container mx-auto px-4 text-center mb-[60px]">
+        <h1 className="text-[24px] lg:text-[42px] font-bold text-[#1A0B5B] font-josefin-sans">
+          Trending Products
+        </h1>
+      </div>
 
-     {/* card div */}
-     <div className="container w-full mx-auto mb-[53px]">
-    <div className='mx-auto grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4'>
-    <TrendingCard loop={TrendingCards}/>
-    </div>
-    </div>
-     
-     {/* Trending bottom card */}
-    <div className='container lg:mx-auto px-4 mt-[39px] flex lg:flex-row flex-col '>
-        <div className='relative h-[270px] w-[300px] lg:w-[420px] lg:mr-[29px] '>
-            <Image src="/tb1.png" alt="arrow" layout='fill' objectFit='cover'></Image>
+      {/* Card Grid */}
+      <div className="container w-full mx-auto mb-[53px]">
+        <div className="mx-auto grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4 justify-items-center">
+          <TrendingCard loop={TrendingCards} />
+        </div>
+      </div>
+
+      {/* Trending Bottom Cards */}
+      <div className="container mx-auto px-4 mt-[39px] flex lg:flex-row flex-col gap-6">
+        <div className="relative h-[270px] w-[300px] lg:w-[420px]">
+          <Image
+            src="/tb1.png"
+            alt="Trending 1"
+            layout="fill"
+            objectFit="cover"
+          />
         </div>
 
-        <div className='relative h-[270px] w-[300px] lg:w-[420px] lg:mr-[24px]'>
-            <Image src="/tb2.png" alt="arrow" layout='fill' objectFit='cover'></Image>
+        <div className="relative h-[270px] w-[300px] lg:w-[420px]">
+          <Image
+            src="/tb2.png"
+            alt="Trending 2"
+            layout="fill"
+            objectFit="cover"
+          />
         </div>
 
-        <div className='h-[270px] w-[267px] '>
-            <div className='relative h-[74px] w-[267px] mb-[20px]'>
-            <Image src="/tp1.png" alt="arrow" layout='fill' objectFit='cover'></Image>
-            </div>
-
-            <div className='relative h-[74px] w-[267px] mb-[20px]'>
-            <Image src="/tp2.png" alt="arrow" layout='fill' objectFit='cover'></Image>
-            </div>
-
-            <div className='relative h-[74px] w-[267px] '>
-            <Image src="/tp3.png" alt="arrow" layout='fill' objectFit='cover'></Image>
-            </div>
+        <div className="h-auto w-[300px] lg:w-[267px] flex flex-col gap-4 items-center">
+          <div className="relative h-[74px] w-full">
+            <Image
+              src="/tp1.png"
+              alt="Trending Small 1"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+          <div className="relative h-[74px] w-full">
+            <Image
+              src="/tp2.png"
+              alt="Trending Small 2"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+          <div className="relative h-[74px] w-full">
+            <Image
+              src="/tp3.png"
+              alt="Trending Small 3"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
         </div>
- 
-    </div>
-
+      </div>
     </section>
-  )
+  );
 }
